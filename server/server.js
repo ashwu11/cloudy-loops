69f58786a9cef186d166c2b4072eef9f1e7ad7ca
Ashley Wu <ashleywu2004@gmail.com>
2024-10-06 23:53:33 -0700

    set up server side

--- server/server.js
+++ server/server.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 // connect to database
-mongoose.connect('mongodb+srv://cloudyloops:1CE6x50pEm04qaa0@cluster0.df06p.mongodb.net/')
+mongoose.connect('mongodb+srv://cloudyloops:cloudy._.loops11@cluster0.df06p.mongodb.net/')
     .then(() => console.log('MongoDB connected'))
     .catch((error) => console.log(error));
 
 // create app
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
+
+
+app.use(
+    cors({
+        origin : 'http://localhost:5173/',
+        methods : ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders : [
+            'Content-Type',
+            'Authorization',
+            'Cache-Control',
+            'Expires',
+            'Pragma'
+        ],
+        credentials : true
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
